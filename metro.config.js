178eeee5d7752dc659053babbbac998b668da5e6
narim <ehdrkd648@naver.com>
2025-09-10 15:10:11 +0900

    Fix GitHub Pages paths with post-build script

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// GitHub Pages를 위한 baseUrl 설정
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+config.resolver.alias = {
+  ...config.resolver.alias,
+};
+
+// Web 빌드를 위한 publicPath 설정
+if (process.env.EXPO_PLATFORM === 'web') {
+  config.resolver.publicPath = '/frontend_mobile/';
+}
+
 module.exports = config;
